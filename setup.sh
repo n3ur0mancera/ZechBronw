d7f02f66fa53aabf30aa28f4486b687badb806e7
n3ur0mancera <j0g8voy14q000@126.com>
2021-06-14 22:39:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-echo "Please Choose: "
-echo " 1. Ubuntu"
-echo " 2. Termux"
-echo " 3. Other"
-read zCh
-
-if [ $zCh = 1 ]; then
-apt-get install git -y
-apt-get install wget -y
-apt-get install curl -y
-
-elif [ $zCh = 2 ]; then
-pkg install git -y
-pkg install wget -y
-pkg install curl -y
-
-elif [ $zCh = 3 ]; then
-apt install git -y
-apt install wget -y
-apt install curl -y
-
-else
-echo "Wrong input"
+# identify system package manager ( PM means [P]ackage [M]anager )
+if [ -e /data/data/com.termux/files/usr/bin/pkg ]; then PM=pkg; SUDO="" 
+elif [ -e /bin/apt ]; then PM=apt; SUDO="sudo"
+elif [ -e /bin/apt-get ]; then PM=apt-get; SUDO="sudo"
+elif [ -e /bin/dnf ]; then PM=dnf; SUDO="sudo"
+elif [ -e /bin/yum ]; then PM=yum; SUDO="sudo"
+elif [ -e /bin/zypp ]; then PM=zypper; SUDO=
+elif [ -e usr/local/bin/brew ]; then PM=brew; SUDO=""
+else echo "
+Sorry I don't know what to do.
+But it's not a problem, because you just have to:
+1: Install packages: git, wget, curl
+2: Type this in your terminal: chmod +x zVirus && chmod +x update.sh
+(to make this files executable)
+"; exit
 fi
+
+# install needed packages with system package manager
+$SUDO $PM install git -y
+$SUDO $PM install wget -y
+$SUDO $PM install curl -y
+
 chmod +x update.sh
 chmod +x zVirus
